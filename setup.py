50d0efc9e3cd8ee3fe9b62189b5f4e5b76846fd0
Jeffrey Marvin Forones <aiscenblue@gmail.com>
2018-08-12 06:50:05 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='py-validator',
+    version='0.0.1',
+    description='JSON Validator for python',
+    author='Jeffrey Marvin Forones',
+    author_email='aiscenblue@gmail.com',
+    license='MIT',
+    url='https://github.com/aiscenblue/py-validator',
+    keywords=['python-validator, py-validator, JSON serializer', 'parameter serializer'],  # arbitrary keywords
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
